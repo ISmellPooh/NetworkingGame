ac7128fac0a09a221341c0f9fc6ae6e453de52a4
Donald <dmoses20@scienceleadership.org>
2019-02-01 10:00:59 -0500

    Scenebuilder with IDed canvas

--- src/org/sla/Controller.java
+++ src/org/sla/Controller.java
@@ -1,25 +1,7 @@
 package org.sla;
 
 public class Controller {
-    int heroX, heroY;
-    int newHeroX, newHeroY;
 
-       if (keyPressed) {
-        if ((key == CODED) && (keyCode == UP)) {
-            newHeroY = heroY - 2;
-        }
-        if ((key == CODED) && (keyCode == DOWN)) {
-            newHeroY = heroY + 2;
-        }
-        if ((key == CODED) && (keyCode == LEFT)){
-            newHeroX = heroX - 2;
-        }
-        if ((key == CODED) && (keyCode == RIGHT)){
-            newHeroX = heroX + 2;
-        }
-
-    }
-
-
+    
 
 }
