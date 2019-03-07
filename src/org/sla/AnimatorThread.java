efcacc3e430451d68b0914b3288347110835df77
ISmellPooh <egorski19@scienceleadership.org>
2019-03-07 10:10:32 -0500

    Fixed down movement problem

--- src/org/sla/AnimatorThread.java
+++ src/org/sla/AnimatorThread.java
@@ -13,7 +13,7 @@ public class AnimatorThread implements Runnable {
             System.out.println("I DREW");
             myController.draw();
             try {
-                Thread.sleep(3000);
+                Thread.sleep(1000);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
